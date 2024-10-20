b15f5f22656a64854619e8356ec18b3122812f72
lowellteddy <lowellteddy88@gmail.com>
2024-10-20 22:57:51 +0300

    fixed the gsap animation and arrangement of the images in the project section

--- jocala/src/components/Projects.js
+++ jocala/src/components/Projects.js
@@ -27,67 +27,28 @@ const Projects = () => {
       }
     });
 
-    // Animate left images
-    gsap.set('.pbox-left img', {
-      x: -100,
-      opacity: 0
-    });
+    // Animate images
+    const imageClasses = ['.img-1', '.img-2', '.img-3', '.img-4'];
+    imageClasses.forEach((imgClass, index) => {
+      gsap.set(imgClass, {
+        x: index % 2 === 0 ? -100 : 100,
+        opacity: 0
+      });
 
-    gsap.to('.pbox-left img', {
-      duration: 1.5,
-      x: 0,
-      opacity: 1,
-      ease: 'power2.out',
-      scrollTrigger: {
-        trigger: '.projects-section',
-        start: 'top center',
-        end: 'bottom center',
-        once: true,
-        markers: false,
-        scrub: 1
-      }
-    });
-
-    // Animate right images
-    gsap.set('.pbox-right img', {
-      x: 100,
-      opacity: 0
-    });
-
-    gsap.to('.pbox-right img', {
-      duration: 1.5,
-      x: 0,
-      opacity: 1,
-      ease: 'power2.out',
-      scrollTrigger: {
-        trigger: '.projects-section',
-        start: 'top center',
-        end: 'bottom center',
-        once: true,
-        markers: false,
-        scrub: 1
-      }
-    });
-
-    // Animate central image with text
-    gsap.set('.project-content img', {
-      y: 100,
-      opacity: 0
-    });
-
-    gsap.to('.project-content img', {
-      duration: 1.5,
-      y: 0,
-      opacity: 1,
-      ease: 'power2.out',
-      scrollTrigger: {
-        trigger: '.projects-section',
-        start: 'top center',
-        end: 'bottom center',
-        once: true,
-        markers: false,
-        scrub: 1
-      }
+      gsap.to(imgClass, {
+        duration: 1.5,
+        x: 0,
+        opacity: 1,
+        ease: 'power2.out',
+        scrollTrigger: {
+          trigger: '.projects-section',
+          start: 'top center',
+          end: 'bottom center',
+          once: true,
+          markers: false,
+          scrub: 1
+        }
+      });
     });
   }, []);
 
@@ -100,70 +61,68 @@ const Projects = () => {
     <section className="projects-section white-bg position-relative overflow-hidden">
       <h2 className="title text-center">Projects</h2>
       <div className="container">
-        <div className="row align-items-stretch overflow-hidden gy-1 gy-md-0 gx-1 gx-md-3 gx-lg-4">
-          <div className="col-md-9">
-            <div className="row g-1 g-md-3 g-lg-4 overflow-hidden">
-              <div className="col-8 pbox-left">
-                <div className="project-box">
-                  <img
-                    src="https://www.yudiz.com/codepen/interior-design/project-01.jpg"
-                    className="img-fluid"
-                    alt="Project"
-                  />
-                </div>
-              </div>
-              <div className="col-4 pbox-right">
-                <div className="project-box">
-                  <img
-                    src="https://www.yudiz.com/codepen/interior-design/project-02.jpg"
-                    className="img-fluid"
-                    alt="Project"
-                  />
-                </div>
-              </div>
-              <div className="col-4 pbox-left">
-                <div className="project-box">
-                  <img
-                    src="https://www.yudiz.com/codepen/interior-design/project-03.jpg"
-                    className="img-fluid"
-                    alt="Project"
-                  />
-                </div>
-              </div>
-              <div className="col-8 overflow-hidden">
-                <div className="project-box project-content">
-                  <img
-                    src="https://www.yudiz.com/codepen/interior-design/project-bg.jpg"
-                    className="img-fluid bg-img"
-                    alt="Project"
-                  />
-                  <div className="row align-items-center h-100">
-                    <div className="col-10 col-md-8 col-xxl-7 ms-auto">
-                      <p>
-                        We celebrated our 100th anniversary with a series of
-                        events and initiatives that paid tribute to our rich
-                        history, our role in the development of the field, and
-                        the great opportunities for the future. But we're only
-                        just beginning. SID's future is exciting, and we're
-                        looking forward to the next 100 years.
-                      </p>
-                      <a
-                        href="#"
-                        onClick={handleReadMoreClick}
-                        className="common-btn ms-auto d-table"
-                      >
-                        <img
-                          src="https://www.yudiz.com/codepen/interior-design/arrow-right.svg"
-                          className="img-fluid"
-                          alt="Arrow"
-                        />
-                      </a>
-                    </div>
-                  </div>
+        <div className="grid-container">
+          <div className="grid-item img-1">
+            <div className="project-box">
+              <img
+                src="https://www.yudiz.com/codepen/interior-design/project-01.jpg"
+                className="img-fluid"
+                alt="Project"
+              />
+            </div>
+          </div>
+          <div className="grid-item img-2">
+            <div className="project-box">
+              <img
+                src="https://www.yudiz.com/codepen/interior-design/project-02.jpg"
+                className="img-fluid"
+                alt="Project"
+              />
+            </div>
+          </div>
+          <div className="grid-item img-3">
+            <div className="project-box">
+              <img
+                src="https://www.yudiz.com/codepen/interior-design/project-03.jpg"
+                className="img-fluid"
+                alt="Project"
+              />
+            </div>
+          </div>
+          <div className="grid-item img-4">
+            <div className="project-box project-content">
+              <img
+                src="https://www.yudiz.com/codepen/interior-design/project-bg.jpg"
+                className="img-fluid bg-img"
+                alt="Project"
+              />
+              <div className="row align-items-center h-100">
+                <div className="col-10 col-md-8 col-xxl-7 ms-auto">
+                  <p>
+                    We celebrated our 100th anniversary with a series of
+                    events and initiatives that paid tribute to our rich
+                    history, our role in the development of the field, and
+                    the great opportunities for the future. But we're only
+                    just beginning. SID's future is exciting, and we're
+                    looking forward to the next 100 years.
+                  </p>
+                  <a
+                    href="#"
+                    onClick={handleReadMoreClick}
+                    className="common-btn ms-auto d-table"
+                  >
+                    <img
+                      src="https://www.yudiz.com/codepen/interior-design/arrow-right.svg"
+                      className="img-fluid"
+                      alt="Arrow"
+                    />
+                  </a>
                 </div>
               </div>
             </div>
           </div>
+        </div>
+        <div className="row">
           <div className="col-md-3 align-self-center">
             <div className="py-lg-5" id="projectDescription">
               <div className="pb-3 paragraph">
