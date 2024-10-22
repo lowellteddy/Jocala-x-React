704a32c90ff4aba7ef88bdb4386e753d38d58e00
lowellteddy <lowellteddy88@gmail.com>
2024-10-22 23:13:41 +0300

    new pathing

--- jocala/src/components/Services.js
+++ jocala/src/components/Services.js
@@ -21,13 +21,11 @@ const Services = () => {
       scrollTrigger: {
         trigger: '.services-section',
         start: 'top center',
-        end: 'center',
         once: true,
         markers: false
       }
     });
 
-    
     const imageClasses = [
       '.img-livingroom-1', '.img-livingroom-2', '.img-livingroom-3', '.img-livingroom-4',
       '.img-kitchen-1', '.img-kitchen-2', '.img-kitchen-3', '.img-kitchen-4',
@@ -58,13 +56,12 @@ const Services = () => {
     });
   }, []);
 
-
   return (
     <section className="services-section white-bg position-relative overflow-hidden">
       <h2 className="title text-center">Services</h2>
       <div className="container">
         {/* Living Room Section */}
-        <h2 className="title text-center">Services</h2>
+        <h3 className="subtitle text-center">Living Room</h3>
         <div className="grid-container">
           <div className="grid-item img-livingroom-1">
             <div className="service-box">
@@ -104,15 +101,11 @@ const Services = () => {
           </div>
         </div>
         <p className="description">
-          <h1>LIVING ROOM</h1>
-        The living room is the heart of the home, a space where family and friends gather to relax and connect.
-        Our interior design team meticulously curates each living room with a perfect blend of comfort and elegance. 
-        We focus on creating an inviting ambiance with plush seating, warm lighting, and tasteful decor.
-        The use of high-quality materials and attention to detail ensures that every element, from the soft textures of the cushions to the sophisticated lines of the furniture, contributes to a cohesive and luxurious environment.
-        Our commitment to delivering the best results is evident in the harmonious balance of style and functionality, making the living room a sanctuary of tranquility and joy.
+          The living room is the heart of the home, a space where family and friends gather to relax and connect. Our interior design team meticulously curates each living room with a perfect blend of comfort and elegance. We focus on creating an inviting ambiance with plush seating, warm lighting, and tasteful decor. The use of high-quality materials and attention to detail ensures that every element, from the soft textures of the cushions to the sophisticated lines of the furniture, contributes to a cohesive and luxurious environment. Our commitment to delivering the best results is evident in the harmonious balance of style and functionality, making the living room a sanctuary of tranquility and joy.
         </p>
 
         {/* Kitchen Section */}
+        <h3 className="subtitle text-center">Kitchen</h3>
         <div className="grid-container">
           <div className="grid-item img-kitchen-1">
             <div className="service-box">
@@ -152,18 +145,11 @@ const Services = () => {
           </div>
         </div>
         <p className="description">
-          <h1>KITCHEN</h1>
-        
-        The kitchen is the hub of culinary creativity and social interaction. 
-        Our designs transform this essential space into a stylish and efficient masterpiece.
-        We prioritize functionality without compromising on aesthetics, integrating state-of-the-art appliances seamlessly with elegant cabinetry and countertops. 
-        The layout is thoughtfully designed to enhance workflow, ensuring that every utensil and ingredient is within easy reach. 
-        Whether it's a cozy breakfast nook or a grand kitchen island, we pay attention to every detail, ensuring that the kitchen is not only a place to cook but also a delightful area to gather and entertain. 
-        Our dedication to excellence shines through in every aspect of our kitchen designs, reflecting our passion for creating spaces that inspire and delight.
-          
+          The kitchen is the hub of culinary creativity and social interaction. Our designs transform this essential space into a stylish and efficient masterpiece. We prioritize functionality without compromising on aesthetics, integrating state-of-the-art appliances seamlessly with elegant cabinetry and countertops. The layout is thoughtfully designed to enhance workflow, ensuring that every utensil and ingredient is within easy reach. Whether it's a cozy breakfast nook or a grand kitchen island, we pay attention to every detail, ensuring that the kitchen is not only a place to cook but also a delightful area to gather and entertain. Our dedication to excellence shines through in every aspect of our kitchen designs, reflecting our passion for creating spaces that inspire and delight.
         </p>
 
         {/* Bedroom Section */}
+        <h3 className="subtitle text-center">Bedroom</h3>
         <div className="grid-container">
           <div className="grid-item img-bedroom-1">
             <div className="service-box">
@@ -203,22 +189,16 @@ const Services = () => {
           </div>
         </div>
         <p className="description">
-          <h1>BEDROOM</h1>
-          
-        The bedroom is a personal retreat, a space for rest and rejuvenation. 
-        Our design approach focuses on creating a serene and comfortable environment that promotes relaxation and peace. 
-        We carefully select soothing color palettes, luxurious fabrics, and bespoke furnishings to craft a bedroom that feels both intimate and opulent. 
-        The placement of lighting, the softness of the bedding, and the thoughtful incorporation of storage solutions all contribute to a harmonious and clutter-free space. 
-        Our unwavering commitment to quality and attention to detail ensures that the bedroom is a perfect blend of comfort and sophistication, providing a sanctuary where you can unwind and recharge.
-          
+          The bedroom is a personal retreat, a space for rest and rejuvenation. Our design approach focuses on creating a serene and comfortable environment that promotes relaxation and peace. We carefully select soothing color palettes, luxurious fabrics, and bespoke furnishings to craft a bedroom that feels both intimate and opulent. The placement of lighting, the softness of the bedding, and the thoughtful incorporation of storage solutions all contribute to a harmonious and clutter-free space. Our unwavering commitment to quality and attention to detail ensures that the bedroom is a perfect blend of comfort and sophistication, providing a sanctuary where you can unwind and recharge.
         </p>
 
         {/* Office Section */}
+        <h3 className="subtitle text-center">Office</h3>
         <div className="grid-container">
           <div className="grid-item img-office-1">
             <div className="service-box">
               <img
-                src="../Assets/Images/office.jpg" // Replace with your image
+                src="../Assets/Images/office.jpg"
                 className="img-fluid"
                 alt="Office"
               />
@@ -227,7 +207,7 @@ const Services = () => {
           <div className="grid-item img-office-2">
             <div className="service-box">
               <img
-                src="../Assets/Images/office2.jpg" // Replace with your image
+                src="../Assets/Images/office2.jpg"
                 className="img-fluid"
                 alt="Office"
               />
@@ -236,7 +216,7 @@ const Services = () => {
           <div className="grid-item img-office-3">
             <div className="service-box">
               <img
-                src="../Assets/Images/office3.jpg" // Replace with your image
+                src="../Assets/Images/office3.jpg"
                 className="img-fluid"
                 alt="Office"
               />
@@ -245,7 +225,7 @@ const Services = () => {
           <div className="grid-item img-office-4">
             <div className="service-box">
               <img
-                src="../Assets/Images/office4.jpg" // Replace with your image
+                src="../Assets/Images/office4.jpg"
                 className="img-fluid"
                 alt="Office"
               />
@@ -253,18 +233,11 @@ const Services = () => {
           </div>
         </div>
         <p className="description">
-          <h1>OFFICE</h1>
-          
-        The office is a space where productivity and creativity thrive. 
-        Our interior design team creates offices that are both functional and inspiring. 
-        We emphasize ergonomic furniture, ample natural light, and a layout that enhances focus and efficiency. 
-        The use of sleek, modern materials and innovative design elements creates a professional yet inviting atmosphere. 
-        Whether it's a home office or a corporate workspace, we tailor our designs to meet the specific needs of our clients, ensuring that the space supports their work style and goals. 
-        Our dedication to excellence and our passion for design are evident in every detail, making the office a place where great ideas come to life.
-
-
-         
+          The office is a space for productivity and creativity. Our designs focus on creating a functional yet inspiring environment where work can flourish. We blend ergonomics with aesthetics, selecting furniture and layouts that promote efficiency and comfort. The use of natural light, combined with thoughtful decor elements, creates a motivating atmosphere that enhances focus and creativity. Each office design is tailored to the client's needs, ensuring that the workspace reflects their personality and encourages productivity. Our commitment to crafting innovative office solutions ensures that every detail is designed to support your work and enhance your experience.
         </p>
+      </div>
+      <div className="text-center mt-4">
+        <button className="common-btn">Go Back</button>
       </div>
     </section>
   );
